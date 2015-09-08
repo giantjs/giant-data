f88eab233a1b83209618647827e902ac808823ef
Dan Stocker <dan@kwaia.com>
2015-09-08 21:23:31 +0100

    moves Link, ValueLink, & OpenChain to giant-data from giant-event

---
+++ src/data/ValueLink.test.js
@@ -0,0 +1,20 @@
+/*global giant, Event, module, test, expect, ok, equal, strictEqual, notStrictEqual, deepEqual, raises */
+(function () {
+    "use strict";
+
+    module("ValueLink");
+
+    test("Instantiation", function () {
+        var link = giant.ValueLink.create();
+
+        ok(link.hasOwnProperty('value'), "should add value property");
+    });
+
+    test("Value setter", function () {
+        var link = giant.ValueLink.create(),
+            value = {};
+
+        strictEqual(link.setValue(value), link, "should be chainable");
+        strictEqual(link.value, value, "should set value property");
+    });
+}());
